3056b0fe93b9cebfb1118ff1ba67b7c297069f59
shining013 <nevertheless113@naver.com>
2024-09-09 10:34:51 +0900

    chore

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -37,7 +37,7 @@
 import UserListView from "../components/user/UserListView.vue";
 import UserSignView from "../components/user/UserSignView.vue";
 import MapView from "../components/user/MappingView.vue";
-import RestView from "../components/user/restView.vue";
+import RestView from "../components/user/RestView.vue";
 import PositionListView from "../components/user/PositionListView.vue";
 import PositionAddView from "../components/user/PositionAddView.vue";
 </script>
